6e5d503c7369c9ec2675aaab93eb80cce5d5d65b
Vicki <vicki.boykis@gmail.com>
2023-01-06 11:18:04 -0500

    JSON to Parquet parser

---
+++ jsonparquet/src/main/java/com/vickiboykis/JSONParquetConverter.java
@@ -0,0 +1,65 @@
+package com.vickiboykis;
+
+import org.apache.avro.Schema;
+import org.apache.parquet.hadoop.ParquetWriter;
+import org.apache.parquet.hadoop.metadata.CompressionCodecName;
+import org.apache.parquet.io.OutputFile;
+import org.kitesdk.data.spi.JsonUtil;
+import org.kitesdk.data.spi.filesystem.JSONFileReader;
+import org.apache.parquet.avro.AvroParquetWriter;
+import java.io.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import static org.apache.avro.generic.GenericData.Record;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * Converts a JSON file to Parquet using an Avro schema
+ *
+ */
+public class JSONParquetConverter
+{
+
+    public static Schema getAvroSchema(String jsonString, String schemaName) {
+        return JsonUtil.inferSchema(JsonUtil.parse(jsonString), schemaName);
+    }
+
+    public static String readFileAsString(String file)throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
+
+    public static <T> void main(String[] args ) throws Exception {
+
+        InputStream is = JSONParquetConverter.class.getClassLoader().getResourceAsStream("goodreads_books.json");
+        String jsonString = new String(is.readAllBytes(), StandardCharsets.UTF_8);
+
+        File outputPath = new File("/resources/goodreads_books.avro");
+        InputStream targetStream = new FileInputStream(String.valueOf(is));
+
+        Schema schema = getAvroSchema(jsonString, "mySchema");
+
+        try (JSONFileReader<Record> reader = new JSONFileReader<>(
+                targetStream, schema, Record.class)) {
+
+            reader.initialize();
+
+            long count = 0;
+
+            try (ParquetWriter<Record> writer = AvroParquetWriter
+                    .<Record>builder((OutputFile) outputPath)
+                    .withCompressionCodec(CompressionCodecName.SNAPPY)
+                    .withSchema(schema)
+                    .build()) {
+                for (Record record : reader) {
+                    writer.write(record);
+                    count += 1;
+                    System.out.println(count + "rows converted");
+                }
+            }
+        }
+    }
+}
