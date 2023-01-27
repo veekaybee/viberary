65a4baeb00d17afad608c00ebb124a51c648fe89
Vicki <vicki.boykis@gmail.com>
2023-01-27 16:55:00 -0500

    Modifying Spark to output single string and building vocab

--- spark/word2vec.py
+++ spark/word2vec.py
@@ -1,22 +1,33 @@
-
 # Script for reading in original JSON file and processing to feed into word2vec
+# ./bin/spark-submit /Users/vicki/viberary/viberary/spark/word2vec.py  --master local
 
-from pyspark.sql.functions import split, col,substring,regexp_replace, collect_list, struct, lit
-from pyspark.sql.functions import explode, concat,concat_ws, expr
+from pyspark.sql.functions import (
+    col,
+    regexp_replace,
+    collect_list,
+    lit,
+    explode,
+    concat,
+    concat_ws,
+    expr,
+)
+from pyspark.sql import Row, SparkSession
 from pyspark.sql.types import IntegerType
 
-# Read in JSON
-df_books = spark.read.json("goodreads_books.json")
+spark = SparkSession.builder.master("local").appName("Word2Vec InputData").getOrCreate()
 
-# Set limit to see how much local processing would work
+# Read in JSON
+df_books = spark.read.json("/Users/vicki/viberary/viberary/data/goodreads_books.json")
+
+# Set limit sample for DF modeling
 df = df_books.limit(10000)
 
 # Flatten Nested JSON on popular_shelves
 df2 = df.select("book_id", "popular_shelves")
-df3 = df2.select("book_id",explode("popular_shelves"))
-df4 = df3.select("book_id", "col.count","col.name")
-df4 = df4.withColumnRenamed("count","num_tags")
-df4 = df4.withColumn("count_num",df4.num_tags.cast(IntegerType()))
+df3 = df2.select("book_id", explode("popular_shelves"))
+df4 = df3.select("book_id", "col.count", "col.name")
+df4 = df4.withColumnRenamed("count", "num_tags")
+df4 = df4.withColumn("count_num", df4.num_tags.cast(IntegerType()))
 
 # Filter noisy tags in exploded popular_shelves
 df4 = df4.filter(df4.name != "to-read")
@@ -28,13 +39,19 @@ df4 = df4.withColumn("name_norm_space", concat(lit(" "), col("name_norm")))
 df4 = df4.withColumn("tag_occurrences", expr("repeat(name_norm_space, num_tags)"))
 df5 = df4.select("book_id", "tag_occurrences")
 df6 = df5.groupBy(["book_id"]).agg(collect_list("tag_occurrences"))
-df7 = df6.withColumn("tags_occurrences",concat_ws(" ",col("collect_list(tag_occurrences)"))).withColumnRenamed("book_id","id")
+df7 = df6.withColumn(
+    "tags_occurrences", concat_ws(" ", col("collect_list(tag_occurrences)"))
+).withColumnRenamed("book_id", "id")
 
 # join back to original data to get book_id, title, and description
-df8 = df.join(df7,df.book_id ==  df7.id,"inner").select(col("book_id"),col("title"), col("description"), col("tags_occurrences"))
-df9 = df8.withColumn("word2vec_input",concat_ws(" ",col("title"), col("description"), col("tags_occurrences")))
+df8 = df.join(df7, df.book_id == df7.id, "inner").select(
+    col("book_id"), col("title"), col("description"), col("tags_occurrences")
+)
+df9 = df8.withColumn(
+    "word2vec_input",
+    concat_ws(" ", col("title"), col("description"), col("tags_occurrences")),
+)
 
 # Write out to file
-df10 = df9.select("book_id","word2vec_input")
-df10.write.csv("wor2vec.csv")
-
+df10 = df9.select("word2vec_input")
+df10.write.csv("viberary/viberary/data/wor2vec.csv")
--- word2vec/cbow.py
+++ word2vec/cbow.py
@@ -1,5 +1,38 @@
-import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
+from torchtext.vocab import vocab
 
-# Create Correct PyTorch Dataset from input CSV
+import csv
+from collections import Counter
+
+# TODO: abstract away util from local file dir
+input_file = "/Users/vicki/viberary/viberary/data/word2vec_input.csv"
+
+# Open file
+def read_input_data(input_file) -> list:
+
+    rows = []
+
+    with open(input_file, "rt") as input_file:
+        for row in csv.reader(input_file):
+            rows.append(row)
+
+    # Build vocabulary from file
+
+
+def create_vocabulary_counter(input_data: list[str]) -> Counter:
+
+    counter = Counter()
+
+    for item in input_data:
+        counter.update(item)
+
+    return counter
+
+
+def build_vocab(
+    vocab_counter: Counter, tokens=list[str]
+) -> torchtext.vocab.vocab.Vocab:
+
+    vocab = text.build_vocab_from_iterator(vocab_counter)
+    return vocab
