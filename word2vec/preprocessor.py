09625e4dba663e6756ab4529f4cb320f6407428c
Vicki <vicki.boykis@gmail.com>
2023-02-03 15:51:45 -0500

    First pass at Word2Vec model in PyTorch, converges

--- word2vec/preprocessor.py
+++ word2vec/preprocessor.py
@@ -13,25 +13,24 @@ Preprocess text from a CSV file into format for modeling CBOW or Skipgram
 """
 
 
-class TextPreProcessor():
+class TextPreProcessor:
     def __init__(self) -> None:
 
         # TODO: create utility class for reading relative paths across the project
         self.input_file = "/Users/vicki/viberary/viberary/data/word2vec_input.csv"
 
-        # TODO: specify min vocab here 
-        
+        # TODO: specify min vocab here
+
     def generate_tokens(self):
-        
+
         with open(self.input_file, encoding="utf-8") as f:
             for line in f:
-                line = line.replace("\\","") # Strip extra \\ in input text
+                line = line.replace("\\", "")  # Strip extra \\ in input text
                 yield line.strip().split()
 
+    def build_vocab(self) -> Vocab:
 
-    def build_vocab(self)-> Vocab:
-
-        vocab = build_vocab_from_iterator(self.generate_tokens(), specials=["<unk>"],min_freq=100)
+        vocab = build_vocab_from_iterator(
+            self.generate_tokens(), specials=["<unk>"], min_freq=100
+        )
         return vocab
-        
-
--- word2vec/run_model.py
+++ word2vec/run_model.py
@@ -1,14 +1,17 @@
 from word2vec.train import ModelTrainer
 from word2vec.model import EMBED_DIMENSION
 
-model = ModelTrainer(300,EMBED_DIMENSION)
+model = ModelTrainer(300, EMBED_DIMENSION)
 
-#TESTING
-context = ['People','create','to', 'direct']
-context_vector = model.make_context_vector(context, word_to_ix)
-a = model(context_vector)
+# Epochs/loss
+print(model.train_model())
 
-#Print result
-print(f'Raw text: {" ".join(raw_text)}\n')
-print(f'Context: {context}\n')
-print(f'Prediction: {ix_to_word[torch.argmax(a[0]).item()]}')
+# # TESTING
+# context = ['People', 'create', 'to', 'direct']
+# context_vector = model.make_context_vector(context, word_to_ix)
+# a = model(context_vector)
+#
+# # Print result
+# print(f'Raw text: {" ".join(raw_text)}\n')
+# print(f'Context: {context}\n')
+# print(f'Prediction: {ix_to_word[torch.argmax(a[0]).item()]}')
