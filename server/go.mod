f38a310cbceeb9298b070e5e17d8762fb250a4b0
Vicki <vicki.boykis@gmail.com>
2023-03-25 16:30:23 -0400

    working Redis query storage

--- server/go.mod
+++ server/go.mod
@@ -1,3 +1,10 @@
 module server
 
 go 1.19
+
+require github.com/go-redis/redis v6.15.9+incompatible
+
+require (
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.27.5 // indirect
+)
--- server/webserver.go
+++ server/webserver.go
@@ -2,45 +2,47 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-redis/redis"
 	"log"
 	"net/http"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/hello" {
-		http.Error(w, "404 not found.", http.StatusNotFound)
-		return
-	}
-
-	if r.Method != "GET" {
-		http.Error(w, "Method is not supported.", http.StatusNotFound)
-		return
-	}
-
-	fmt.Fprintf(w, "Hello!")
-}
+var client *redis.Client
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "ParseForm() err: %v", err)
 		return
 	}
-	fmt.Fprintf(w, "POST request successful")
-	name := r.FormValue("name")
-	address := r.FormValue("address")
+	fmt.Fprintf(w, "POST request successful ")
+	query := r.FormValue("query")
 
-	fmt.Fprintf(w, "Name = %s\n", name)
-	fmt.Fprintf(w, "Address = %s\n", address)
+	fmt.Fprintf(w, "query = %s\n", query)
+
+	client.Set(query, 1.0, 0)
 }
 
 func main() {
 
+	// Handle form requests
 	fileServer := http.FileServer(http.Dir("./static"))
+
 	http.Handle("/", fileServer)
 	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("Starting server at port 8080\n")
+
+	// Connect to local Redis
+	client = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "",
+		DB:       0,
+	})
+
+	fmt.Printf("Connecting to Redis\n")
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
