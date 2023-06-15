1349c77569c1af53bb2ebd8b01acaae37586e449
Vicki <vicki.boykis@gmail.com>
2023-06-15 13:27:59 -0400

    update requirements and file_reader with paths

--- src/io/file_reader.py
+++ src/io/file_reader.py
@@ -1,7 +1,13 @@
 import importlib.resources
 from io import TextIOWrapper
+from pathlib import Path
 
 def get_resource(resource)-> TextIOWrapper:
     with importlib.resources.as_file(resource) as path:
-            with path.open() as f:
-                return f
+        f = path.open()
+        return f
+    
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent.parent
