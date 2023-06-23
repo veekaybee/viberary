a87448cd8ecaf4d77bf9f1c57c86f565b97c86ec
Vicki <vicki.boykis@gmail.com>
2023-06-23 15:50:13 -0400

    linting

--- src/models/word2vec/src/run_model.py
+++ src/models/word2vec/src/run_model.py
@@ -19,20 +19,20 @@ writer = SummaryWriter("/tmp/log")
 model = cbow.load()
 
 # Test word prediction
-context = ['third', 'shapeshifter', '2016', 'for']
+context = ["third", "shapeshifter", "2016", "for"]
 context_vector = model.make_context_vector(context, model.word_to_ix)
 a = model(context_vector)
 
 # Return results
-print(f'Raw text: {model.vocab}')
+print(f"Raw text: {model.vocab}")
 print(f"Context: {context}\n")
 print(f"Prediction: {model.ix_to_word[torch.argmax(a[0]).item()]}")
 
 # Check Embeddings Final Weights
-print('Getting weights:\n', model.embeddings.weight.data[1])
+print("Getting weights:\n", model.embeddings.weight.data[1])
 
 # Check Embeddings
-print('Getting embeddings:\n', model.embeddings)
+print("Getting embeddings:\n", model.embeddings)
 
 vectors = model.embeddings.weight
 
