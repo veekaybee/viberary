f3f788e91d53b2376d13094829b2afbc8d7468ac
Vicki <vicki.boykis@gmail.com>
2023-07-03 16:40:24 -0400

    more redis connection tweaks

--- src/inout/redis_conn.py
+++ src/inout/redis_conn.py
@@ -3,7 +3,7 @@ from redis import Redis
 
 class RedisConnection:
     def conn(self) -> Redis:
-        host = "localhost"
+        host = "redis"
         port = 6379
         redis_conn = Redis(host=host, port=port, decode_responses=True)
         return redis_conn
