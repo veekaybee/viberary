e58d7bc65bde882e787061333ff74e7227ba9685
Vicki Boykis <vicki.boykis@gmail.com>
2023-07-23 19:12:28 -0400

    working HTML with FastAPI

---
+++ src/api/main.py
@@ -0,0 +1,47 @@
+import logging.config
+
+from fastapi import FastAPI
+import uvicorn
+import pydantic
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+
+from inout.file_reader import get_root_dir, get_config_file as cf
+from inout.redis_conn import RedisConnection
+from search.knn_search import KNNSearch
+from os import path
+
+conf = cf()
+logging.config.fileConfig(conf["logging"]["path"])
+root = get_root_dir()
+
+app = FastAPI(title="Viberary")
+templates = Jinja2Templates(directory= Path( f"{root}/src/api/templates"))
+app.mount("/static", StaticFiles(directory=f"{root}/src/api/static"), name="static")
+
+class QueryRequest(pydantic.BaseModel):
+    query: str
+
+@app.post("/search")
+async def search(request: QueryRequest):
+    retriever = KNNSearch(RedisConnection().conn())
+    data = retriever.top_knn(request.query)
+    return templates.TemplateResponse("search.html", {"data": data, "query": request.query})
+
+@app.get("/",response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse("index.html", context= {"request": request})
+
+@app.get("/ping")
+async def root():
+    return {"message": "pong"}
+
+@app.get("/how",response_class=HTMLResponse)
+async def how(request: Request):
+    return templates.TemplateResponse("how.html", context={"request": request})
+
+
--- src/inout/file_reader.py
+++ src/inout/file_reader.py
@@ -4,11 +4,16 @@ import git
 import yaml
 
 
-def get_config_file() -> dict:
-    """Access config options"""
+def get_root_dir()->Path:
+    """get root path for project based on git"""
     repo = git.Repo(".", search_parent_directories=True)
     root_dir = repo.working_tree_dir
-    config_path = Path(f"/{root_dir}/config.yml")
+    return root_dir
+
+def get_config_file() -> dict:
+    """Access config options"""
+    root_dir = get_root_dir()
+    config_path = Path( f"{root_dir}/config.yml")
 
     with open(config_path, "r") as file:
         config = yaml.safe_load(file)
