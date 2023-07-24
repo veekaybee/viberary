033cae97ba362e7f798f534c6f9e925dfa1dafa9
Vicki Boykis <vicki.boykis@gmail.com>
2023-07-23 20:49:51 -0400

    Changing index

--- src/api/main.py
+++ src/api/main.py
@@ -1,19 +1,15 @@
 import logging.config
 
-from fastapi import FastAPI
-import uvicorn
-import pydantic
+from pydantic import BaseModel, Json
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
 
-
 from inout.file_reader import get_root_dir, get_config_file as cf
 from inout.redis_conn import RedisConnection
 from search.knn_search import KNNSearch
-from os import path
 
 conf = cf()
 logging.config.fileConfig(conf["logging"]["path"])
@@ -23,11 +19,9 @@ app = FastAPI(title="Viberary")
 templates = Jinja2Templates(directory= Path( f"{root}/src/api/templates"))
 app.mount("/static", StaticFiles(directory=f"{root}/src/api/static"), name="static")
 
-class QueryRequest(pydantic.BaseModel):
-    query: str
 
 @app.post("/search")
-async def search(request: QueryRequest):
+async def search(request: Request):
     retriever = KNNSearch(RedisConnection().conn())
     data = retriever.top_knn(request.query)
     return templates.TemplateResponse("search.html", {"data": data, "query": request.query})
@@ -36,12 +30,11 @@ async def search(request: QueryRequest):
 def index(request: Request):
     return templates.TemplateResponse("index.html", context= {"request": request})
 
-@app.get("/ping")
-async def root():
-    return {"message": "pong"}
 
 @app.get("/how",response_class=HTMLResponse)
 async def how(request: Request):
     return templates.TemplateResponse("how.html", context={"request": request})
 
-
+@app.get("/ping")
+async def root():
+    return {"message": "pong"}
